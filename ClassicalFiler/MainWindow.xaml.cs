a4873f030d9aabebc4d98f855aabd7adff02ab3e
setanamimoi <setanamimoi@gmail.com>
2012-09-01 19:25:59 +0900

    C ドライブの内容を表示

--- ClassicalFiler/MainWindow.xaml.cs
+++ ClassicalFiler/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ClassicalFiler
 {
@@ -22,6 +23,51 @@ namespace ClassicalFiler
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            DirectoryInfo source = new DirectoryInfo(@"C:\");
+
+            List<FileSystemInfo> children = new List<FileSystemInfo>();
+            children.AddRange(source.GetDirectories());
+            children.AddRange(source.GetFiles());
+
+            //this.listView.Columns.Add("Name", "名前");
+            //this.listView.Columns.Add("UpdateTime", "更新日時");
+            //this.listView.Columns.Add("Extentions", "種類");
+            //this.listView.Columns.Add("Size", "サイズ");
+            //this.dataGrid.ItemsSource = children;
+
+            List<object> lst = new List<object>();
+            foreach (FileSystemInfo child in children)
+            {
+                string extentions = System.IO.Path.GetExtension(child.FullName);
+                if (child is DirectoryInfo)
+                {
+                    extentions = "dir";
+                }
+
+                string size = "";
+                FileInfo file = child as FileInfo;
+                if (file != null)
+                {
+                    size = file.Length.ToString();
+                }
+
+                
+                lst.Add(
+                    new  { 
+                        Name = child.Name, 
+                        LastWriteTime = string.Format("{0:yyyy/MM/dd HH:mm:ss}", child.LastWriteTime), 
+                        Extention = extentions,
+                        Length = size,
+                    });
+
+                //this.listView.Items.Add(item);
+            }
+
+            this.dataGrid.ItemsSource = lst;
         }
     }
 }
