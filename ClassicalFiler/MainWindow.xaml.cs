3f06f761191728b4b2f300f05dbdca30f87a03dd
setanamimoi <setanamimoi@gmail.com>
2012-09-09 16:48:17 +0900

    右キーもしくはEnterキーで下層フォルダに移動する機能を追加

--- ClassicalFiler/MainWindow.xaml.cs
+++ ClassicalFiler/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Input;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace ClassicalFiler
 {
@@ -17,7 +20,33 @@ namespace ClassicalFiler
 
         private void Window_Initialized(object sender, EventArgs e)
         {
-            this.dataGrid.ItemsSource = new PathInfo(@"C:\").GetChildren();
+            PathInfo pathInfo = new PathInfo(@"C:\");
+
+            this.dataGrid_OpenDirectory(pathInfo);
+        }
+
+        private void dataGrid_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Enter) || Keyboard.IsKeyDown(Key.Right))
+            {
+                PathInfo selectedPath = this.dataGrid.SelectedItem as PathInfo;
+
+                if (selectedPath == null)
+                {
+                    return;
+                }
+
+                this.dataGrid_OpenDirectory(selectedPath);
+            }
+        }
+
+        private void dataGrid_OpenDirectory(PathInfo pathInfo)
+        {
+            this.dataGrid.ItemsSource = pathInfo.GetChildren();
+            this.dataGrid.Focus();
+            
+            object firstItem = this.dataGrid.Items.Cast<object>().First();
+            this.dataGrid.CurrentCell = new DataGridCellInfo(firstItem, this.dataGrid.Columns.First());
         }
     }
 }
